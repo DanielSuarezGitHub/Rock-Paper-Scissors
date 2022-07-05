896ac659b7f567d42ffc9887a5d8cc7b1799e605
Daniel Suarez <danielsuarezscripts@gmail.com>
2022-07-05 16:22:15 -0700

    add computerPlay function
    Function randomly selects between Rock, Paper, Scissors

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+function computerPlay() {
+ x = Math.floor(Math.random() * 100);
+ if (x < 33) {
+   return "paper"
+ } else if (x < 66) {
+    return "scissors"
+ } else {
+    return "rock"
+ }
+}
+
+function playerSelection() {
+    
+}
